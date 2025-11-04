680d1ee42e359ffa81d3d5f8e8e2ce03e809e0d5
Paulo Cleidson <167731152+Paulo-Cleidson@users.noreply.github.com>
2025-11-04 13:25:53 -0300

    liberação de autorização para teste

--- apps/carteira/api/v1/viewsets.py
+++ apps/carteira/api/v1/viewsets.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets
 from apps.carteira.models import Carteira
 from .serializers import CarteiraSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 class CarteiraViewsets(viewsets.ModelViewSet):
     queryset = Carteira.objects.all()
     serializer_class = CarteiraSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
